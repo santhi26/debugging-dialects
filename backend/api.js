004fcd292a1783a3304b96c7e36dbb45bd2fd36d
elliotclowes <elliot@clowes.me>
2023-07-24 14:53:38 +0100

    getByID route added

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,7 @@
 // Include required packages
 const express = require('express')
 const cors = require('cors')
-// const db = require('./utils/db')
+const db = require('./database/db')
 require('dotenv').config()
 
 // Initialize Express application
@@ -13,23 +13,13 @@ app.use(express.json())
 
 // Import user, chat and flashcard routes
 const userRoutes = require('./Routers/users')
-// const flashcardRoutes = require('./Routers/flashcards')
+const flashcardsRouter = require('./Routers/flashcards')
 // const chatRoutes = require('./Routers/chats')
 
 // Define API routes
 app.use('/api/user', userRoutes)
-// app.use('/api/flashcard', flashcardRoutes)
+app.use('/api/flashcard', flashcardsRouter)
 // app.use('/api/chat', chatRoutes)
 
-
-app.get('/api/test-db', async (req, res) => {
-  try {
-    const result = await db.one('SELECT 1');
-    res.json(result);
-  } catch(err) {
-    console.log(err);
-    res.status(500).json({ error: 'Database connection error' });
-  }
-});
 
 module.exports = app;
