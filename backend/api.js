ca9c388594301cdfa96d70e7ce9c4a26e10d2b66
elliotclowes <elliot@clowes.me>
2023-07-24 09:10:12 +0100

    More setup files added

---
+++ backend/api.js
@@ -0,0 +1,35 @@
+// Include required packages
+const express = require('express')
+const cors = require('cors')
+const db = require('./utils/db')
+require('dotenv').config()
+
+// Initialize Express application
+const app = express()
+
+// Enable CORS and JSON parsing
+app.use(cors())
+app.use(express.json())
+
+// Import user, chat and flashcard routes
+const userRoutes = require('./routes/users')
+const flashcardRoutes = require('./routes/flashcards')
+const chatRoutes = require('./routes/chats')
+
+// Define API routes
+app.use('/api/user', userRoutes)
+app.use('/api/flashcard', flashcardRoutes)
+app.use('/api/chat', chatRoutes)
+
+
+app.get('/api/test-db', async (req, res) => {
+  try {
+    const result = await db.one('SELECT 1');
+    res.json(result);
+  } catch(err) {
+    console.log(err);
+    res.status(500).json({ error: 'Database connection error' });
+  }
+});
+
+module.exports = api;
