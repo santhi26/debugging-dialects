328c7ca6c0d4c5fab1fe88dddd42af2bb0cbc4e3
Santhi26 <shanthipriya.addanki@gmail.com>
2023-07-24 13:49:09 +0100

    added flashcard component

---
+++ frontend/src/components/FlashCard.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const Flashcard = ({ question, answer }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const toggleAnswer = () => {
+    setShowAnswer(!showAnswer);
+  };
+
+  return (
+    <div className="flashcard" onClick={toggleAnswer}>
+      <div className="flashcard-question">
+        <h3>Question:</h3>
+        <p>{question}</p>
+      </div>
+      {showAnswer && (
+        <div className="flashcard-answer">
+          <h3>Answer:</h3>
+          <p>{answer}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Flashcard;
