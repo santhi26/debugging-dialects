027e761472d3cee00e696bdb9f5e7f9553b5fe3e
laisjmvet <laisjmvet@gmail.com>
2023-07-25 14:05:48 +0100

    flashcard

---
+++ frontend/src/components/FlashCardList/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+//import {FlashCard} from '../FlashCard'
+
+export default function FlashCardList({flashCards}) {
+  return (
+    console.log(flashCards)
+    // {flashCards.map(c => <FlashCard key={c.id} />)}
+  )
+}
+
--- frontend/src/pages/flashCard/index.jsx
+++ frontend/src/pages/flashCard/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { GetFlashCard} from '../../components';
 
 
 
-export default function HomePage() {
+export default function flashCard() {
     return (
         <>
             <h1>fluentPal</h1>
             <em>Learn a lanugage</em>
+            <GetFlashCard />
             
         </>
     )
