78edd7987958299e83592fad6c2a4d3532180a58
Oliver Thomas <oliver.thomas20011@gmail.com>
2023-07-26 09:56:57 +0100

    Added inactive user list

--- frontend/src/components/UserList/index.jsx
+++ frontend/src/components/UserList/index.jsx
@@ -4,10 +4,12 @@ import { UserContext } from "../../contexts";
 export default function UserList({setRecipient}) {
     const {users} = useContext(UserContext);
     const [activeUsers, setActiveUsers] = useState([{username:""}]);
+    const [inactiveUsers, setInactiveUsers] = useState([{username:""}]);
 
     //when users is updated, it edits the list of active users.
     useEffect(()=>{
         setActiveUsers(users.filter(acc => acc.is_online === true));
+        setInactiveUsers(users.filter(acc => acc.is_online === false));
     },[users])
 
 
@@ -22,6 +24,12 @@ export default function UserList({setRecipient}) {
             <h2><u>Active Users</u></h2>
             <ul>
                 {activeUsers.map(act => (
+                    <button type='button' key={act.user_id} onClick={changeRecipient}>{act.username}</button>
+                ))}
+            </ul>
+            <h2><u>Inactive Users</u></h2>
+            <ul>
+                {inactiveUsers.map(act => (
                     <button type='button' key={act.user_id} onClick={changeRecipient}>{act.username}</button>
                 ))}
             </ul>
