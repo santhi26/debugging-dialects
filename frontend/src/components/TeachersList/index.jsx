d005229aacbd268a1f6f2c53dbbf5d83312e183c
elliotclowes <elliot@clowes.me>
2023-07-28 10:58:28 +0100

    Styled teacher list page

--- frontend/src/components/TeachersList/index.jsx
+++ frontend/src/components/TeachersList/index.jsx
@@ -1,36 +1,53 @@
-import {useState, useEffect, useContext} from 'react';
-import {useLocation} from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { UserContext } from '../../contexts';
-import {TeacherProfile} from '../'
+import { TeacherProfile } from '../';
 
-
-    
 export default function TeachersList() {
-    const { setRating } = useContext(UserContext);
-    const [data, setData] = useState([])
-    
-    const searchTeachersAPI = async() => {
-        try {
-            const response = await fetch(`http://localhost:3000/api/teacher/allTeachers`);
-            const result = await response.json();
-            setData(result);    
-            setRating(result)       
-            console.log(result, "<<<<<<", result) 
-            
-        } catch (error) {
-            console.log(error)
-        }
+  const { setRating } = useContext(UserContext);
+  const [data, setData] = useState([]);
+
+  const searchTeachersAPI = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/teacher/allTeachers`);
+      const result = await response.json();
+      setData(result);
+      setRating(result);
+      console.log(result, '<<<<<<', result);
+    } catch (error) {
+      console.log(error);
     }
-    useEffect(() =>{
-      searchTeachersAPI()
+  };
 
-    },[])
+  useEffect(() => {
+    searchTeachersAPI();
+  }, []);
 
-    return (
-        <div className="teachers-container">
-			<ul className="teachers-list">
-				{data.map((teacher) => <TeacherProfile data={teacher} key={teacher.teacher_id} />)}
-			</ul>
-		</div>
-    )
+  return (
+    <>
+      <div class="general wf-section">
+        <div class="overview fluentcontent wf-section">
+          <div class="content-wrapper-m-copy center content-section-title">
+            <div class="w-richtext">
+            <h1 class="page-title">Meet the Teachers</h1>
+              <section id="our-team" class="teacher-section wf-section">
+                <section class="content-wrapper-centered-about">
+                  <div class="title-wrapper-jobs-v1">
+                    <p class="heading-paragraph">
+                      Below is some of our top teachers. They've been hand-picked by our team to ensure you get the best learning experience possible.
+                    </p>
+                  </div>
+                  <div class="team-content-wrap">
+                    <div class="w-layout-grid grid-2">
+                      {data.map((teacher) => <TeacherProfile data={teacher} key={teacher.teacher_id} />)}
+                    </div>
+                  </div>
+                </section>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
