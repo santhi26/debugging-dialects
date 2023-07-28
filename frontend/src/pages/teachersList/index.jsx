720f2f16977b88660b8c4cb3ca313b48a5ae9a77
laisjmvet <laisjmvet@gmail.com>
2023-07-28 08:15:46 +0100

    page see all teachers ok

--- frontend/src/pages/teachersList/index.jsx
+++ frontend/src/pages/teachersList/index.jsx
@@ -4,8 +4,7 @@ export default function teachersList() {
 
     return (
         <>
-            <Component.TeachersList />          
-            {/* <Component.TeacherProfile />   */}
+            <Component.TeachersList />
         </>
     )
 };
